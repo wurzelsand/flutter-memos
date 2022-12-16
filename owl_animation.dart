571f3240388d033510419d0cc8488b911cbb4987
wurzelsand <wurzelsand@gmail.com>
2022-12-16 16:47:24 +0100

    something changed

--- owl_animation.dart
+++ owl_animation.dart
@@ -1,87 +1,10 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 
-enum Opacity {
-  opaque(raw: 1.0),
-  transparent(raw: 0.0);
-
-  const Opacity({required this.raw});
-
-  final double raw;
-}
-
-class FadeLabel {
-  const FadeLabel({required this.label, required this.opacity});
-
-  final String label;
-  final Opacity opacity;
-}
-
-class FadeLabelCubit extends Cubit<FadeLabel> {
-  FadeLabelCubit({required FadeLabel fadeLabel}) : super(fadeLabel);
-
-  void fadeIn() =>
-      emit(FadeLabel(label: state.label, opacity: Opacity.opaque));
-
-  void fadeOut() =>
-      emit(FadeLabel(label: state.label, opacity: Opacity.transparent));
-
-  void setLabel(String label) =>
-      emit(FadeLabel(label: label, opacity: state.opacity));
-}
-
-const owlUrl =
-    'https://raw.githubusercontent.com/flutter/website/master/src/images/owl.jpg';
-
-class FadeInDemo extends StatelessWidget {
-  const FadeInDemo({Key? key}) : super(key: key);
-
-  @override
-  Widget build(BuildContext context) {
-    return BlocBuilder<FadeLabelCubit, FadeLabel>(
-      builder: (context, state) {
-        return Column(children: <Widget>[
-          Image.network(owlUrl),
-          TextButton(
-              child: Text(
-                state.label,
-                style: const TextStyle(color: Colors.blueAccent),
-              ),
-              onPressed: () => state.opacity == Opacity.transparent
-                  ? context.read<FadeLabelCubit>().fadeIn()
-                  : context.read<FadeLabelCubit>().fadeOut()),
-          AnimatedOpacity(
-            duration: const Duration(seconds: 2),
-            opacity: state.opacity.raw,
-            onEnd: () => state.opacity == Opacity.transparent
-                ? context.read<FadeLabelCubit>().setLabel('Show Details')
-                : context.read<FadeLabelCubit>().setLabel('Hide Details'),
-            child: Column(
-              children: const [
-                Text('Type: Owl'),
-                Text('Age: 39'),
-                Text('Employment: None'),
-              ],
-            ),
-          )
-        ]);
-      },
-    );
-  }
-}
-
-class MyPage extends StatelessWidget {
-  const MyPage({Key? key}) : super(key: key);
-
-  @override
-  Widget build(BuildContext context) {
-    return BlocProvider(
-      create: (_) => FadeLabelCubit(
-          fadeLabel: const FadeLabel(
-              label: 'Show Details', opacity: Opacity.transparent)),
-      child: const FadeInDemo(),
-    );
-  }
+void main() {
+  runApp(
+    const MyApp(),
+  );
 }
 
 class MyApp extends StatelessWidget {
@@ -99,8 +22,90 @@ class MyApp extends StatelessWidget {
   }
 }
 
-void main() {
-  runApp(
-    const MyApp(),
-  );
+class MyPage extends StatelessWidget {
+  const MyPage({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return MultiBlocProvider(
+      providers: [
+        BlocProvider<FadeCubit>(create: (_) => FadeCubit(Opacity.transparent)),
+        BlocProvider<LabelCubit>(create: (_) => LabelCubit('Show Details')),
+      ],
+      child: const FadeInDemo(),
+    );
+  }
+}
+
+const owlUrl =
+    'https://raw.githubusercontent.com/flutter/website/master/src/images/owl.jpg';
+
+class FadeInDemo extends StatelessWidget {
+  const FadeInDemo({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return BlocBuilder<FadeCubit, Opacity>(
+      builder: (_, opacity) {
+        return BlocBuilder<LabelCubit, String>(
+          builder: (_, label) {
+            return Column(children: <Widget>[
+              Image.network(owlUrl),
+              TextButton(
+                child: Text(
+                  label,
+                  style: const TextStyle(color: Colors.blueAccent),
+                ),
+                onPressed: () => opacity == Opacity.transparent
+                    ? context.read<FadeCubit>().fadeIn()
+                    : context.read<FadeCubit>().fadeOut(),
+              ),
+              BlocBuilder<FadeCubit, Opacity>(
+                builder: (context, opacity) {
+                  return AnimatedOpacity(
+                    duration: const Duration(seconds: 2),
+                    opacity: opacity.raw,
+                    onEnd: () => context.read<LabelCubit>().set(
+                        opacity == Opacity.transparent
+                            ? 'Show Details'
+                            : 'Hide Details'),
+                    child: Column(
+                      children: const [
+                        Text('Type: Owl'),
+                        Text('Age: 39'),
+                        Text('Employment: None'),
+                      ],
+                    ),
+                  );
+                },
+              )
+            ]);
+          },
+        );
+      },
+    );
+  }
+}
+
+enum Opacity {
+  opaque(raw: 1.0),
+  transparent(raw: 0.0);
+
+  const Opacity({required this.raw});
+
+  final double raw;
+}
+
+class FadeCubit extends Cubit<Opacity> {
+  FadeCubit(Opacity opacity) : super(opacity);
+
+  void fadeIn() => emit(Opacity.opaque);
+
+  void fadeOut() => emit(Opacity.transparent);
+}
+
+class LabelCubit extends Cubit<String> {
+  LabelCubit(String initial) : super(initial);
+
+  void set(String label) => emit(label);
 }
